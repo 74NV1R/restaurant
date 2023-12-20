0f3f0a5562bbf70cedf46bc5cd80dc9b5b444f0d
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2023-12-20 09:54:50 +0600

    server connected

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,17 +1,46 @@
-import DISHES from '../data/dishes'
 import * as actionTypes from './actionTypes'
+import axios from 'axios'
+import { baseUrl } from './baseUrl'
 
 
 
-export const addComment = (dishId, rating, author, comment) => ({
-    type: actionTypes.ADD_COMMENT,
-    payload: {
+export const addComment = (dishId, rating, author, comment) => dispatch => {
+    const newComment = {
         dishId: dishId,
         author: author,
         rating: rating,
         comment: comment
     }
+    newComment.date = new Date().toISOString()
+
+    axios.post(baseUrl + 'comments', newComment)
+        .then(response => response.data)
+        .then(comment => dispatch(commentConcat(comment)))
+}
+
+export const commentConcat = (comment) => ({
+    type: actionTypes.ADD_COMMENT,
+    payload: comment
 })
+
+export const commentLoading = () => ({
+    type: actionTypes.COMMENT_LOADING
+})
+
+export const LoadComments = comments => ({
+    type: actionTypes.LOAD_COMMENTS,
+    payload: comments
+})
+
+export const fetchComments = () => dispatch => {
+
+    dispatch(commentLoading())
+
+    axios.get(baseUrl + 'comments')
+        .then(response => response.data)
+        .then(comments => dispatch(LoadComments(comments)))
+}
+
 
 
 export const loadDishes = dishes => ({
@@ -23,12 +52,11 @@ export const dishesLoading = () => ({
     type: actionTypes.DISHES_LOADING
 })
 
-export const fetchDishes = () => {
-    return dispatch => {
-        dispatch(dishesLoading())
-        setTimeout(() => {
-            dispatch(loadDishes(DISHES))
-        }, 2000);
+export const fetchDishes = () => dispatch => {
 
-    }
+    dispatch(dishesLoading())
+
+    axios.get(baseUrl + 'dishes')
+        .then(response => response.data)
+        .then(dishes => dispatch(loadDishes(dishes)))
 }
